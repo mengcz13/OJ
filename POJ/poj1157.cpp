1c05c088acd6333960eba21652b8cf61e20754dc
mengcz13 <mengcz13@mails.tsinghua.edu.cn>
2015-10-08 22:38:41 +0800

    增加了1157的注释

--- POJ/poj1157.cpp
+++ POJ/poj1157.cpp
@@ -29,12 +29,15 @@ int main(){
     for (int i=2;i<=f;++i)
         for (int j=i;j<=v;++j){
             tempmax[j]=value[i][j];
+            //j-i时间内计算从位置q到末端j的最大值
             for (int q=j-1;q>=i;q--){
                 if (value[i][q]>tempmax[q+1])
                     tempmax[q]=value[i][q];
                 else
                     tempmax[q]=tempmax[q+1];
             }
+            //状态转移方程ae[i][j]=max{ae[i-1][k]+max{value[i][k+1..j]}}(i-1<=k<=j-1, i<=j<=v)
+            //ae[i][j]表示前i种花插在前j个花瓶内所能得到的最大值, 注意每种花都不能不插, 所以j>=i
             int cum=ae[i-1][i-1]+tempmax[i], temp=0;
             for (int k=i;k<j;++k){
                 temp=ae[i-1][k]+tempmax[k+1];
